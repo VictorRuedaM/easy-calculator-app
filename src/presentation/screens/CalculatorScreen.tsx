2c08b336d6cee87b3b719b20e752a6567999c9b9
Victor Rueda <vialruma1985@gmail.com>
2024-03-12 13:06:33 -0500

    Changes to be committed: 	modified:   src/config/theme/app-theme.ts 	modified:   src/presentation/screens/CalculatorScreen.tsx

--- src/presentation/screens/CalculatorScreen.tsx
+++ src/presentation/screens/CalculatorScreen.tsx
@@ -1,4 +1,4 @@
-import {Text, View} from 'react-native';
+import {Pressable, Text, View} from 'react-native';
 import {globalStyles} from '../../config/theme/app-theme';
 
 export const CalculatorScreen = () => {
@@ -7,6 +7,11 @@ export const CalculatorScreen = () => {
       <View style={globalStyles.rowResult}>
         <Text style={globalStyles.mainResult}>1500</Text>
         <Text style={globalStyles.subResult}>15</Text>
+      </View>
+      <View style={globalStyles.row}>
+        <Pressable style={globalStyles.button}>
+          <Text style={globalStyles.buttonText}>5</Text>
+        </Pressable>
       </View>
     </View>
   );
